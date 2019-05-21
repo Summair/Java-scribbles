48f4ee59203e23bb0cf66ab9ea28f0045b93d348
SummairB <48170270+Summair@users.noreply.github.com>
2019-05-20 19:51:17 -0500

    Add files via upload
    temprature converter

---
+++ TempConverter.java
@@ -0,0 +1,44 @@
+/* Mohammed Brohi
+Course CIS144
+Date: Feb 4 2019
+Assignment: Temprature conversion
+Description: Simple to print out data */
+
+
+
+
+// Assignment Temprature converter
+//Class import for inputs Java
+
+
+
+
+mport java.util.Scanner; // using Scanner
+
+public class TempConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Create Scanner to obtain input values from command window
+		
+		Scanner input = new Scanner(System.in);
+		
+		String name;
+		double F_number;
+		double Celsius;
+				
+	System.out.print("Please enter your name : ");
+	    name= input.nextLine(); //Read name here
+	
+	System.out.print("Enter temprature in Farenheit:");
+	F_number = input.nextDouble();  //line for Farenheit input
+	
+	Celsius =  5.0/9.0 * (F_number - 32.0); //Celsius formula
+	
+	System.out.println ("This program is authored by" + name );
+	System.out.printf ("For a Fahrenehit temprature of %.2f Degrees the equivalent in Celsius temprature is %.2f degree" , F_number,Celsius);
+	
+	}
+	
+
+}
