a4289236b2d4a2485761790d8e20733bc018bc57
Summair <48170270+Summair@users.noreply.github.com>
2019-08-09 14:46:13 -0500

    Add files via upload

---
+++ echo server/src/SimpleEchoServer.java
@@ -0,0 +1,39 @@
+/* Simple Echo server demo 
+ * M.Brohi
+ */
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class SimpleEchoServer {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Simple Echo Server");
+	
+		try (ServerSocket serverSocket = new ServerSocket(6000) ) {
+			System.out.println("Waiting for connection....");
+			Socket clientSocket = serverSocket.accept();
+			System.out.println("Connected to client");
+		} catch (IOException ex) {
+			// Handle exceptions
+		}
+		
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream));
+				PrintWriter out = new PrintWriter (clientSocket.getOutputStream(), true)) {
+					String inputLine;
+					while ((inputLine = br.readLine()) !=null) {
+						System.out.println("Server: " + inputLine);
+						out.println(inputLine);
+					}
+				}
+	}
+
+}
